6686c12bb7ed703c82cf9a4ab497501b6033eb93
Haowei Hsu <hwhsu1231@localizethedocs.org>
2026-07-05 20:11:21 +0800

    WIP

--- docs/conf.py
+++ docs/conf.py
@@ -294,7 +294,7 @@ html_experimental_html5_writer = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "basic"
+html_theme = "shibuya"
 
 html_context = {
     "discord_invite": "https://discord.gg/disnake",
